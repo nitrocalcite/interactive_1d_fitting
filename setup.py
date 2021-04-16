5de550d5da4178a36169431e67056e074a763068
Shane Caldwell <caldwell.shane@gmail.com>
2021-04-15 22:57:53 -0700

    Switch to Poetry for project management

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(name="lmf_gui",
-      version="0.0.1-beta",
-      description="An interactive 1D fitting toolkit",
-      author="Jonathan Okasinski",
-      packages=['lmf_gui'])
