f7e924c49a5216a2f04cd0ce84d3cbbc6451fe12
Jonathan Okasinski <jokasinski@rigetti.com>
2021-04-16 13:43:09 -0700

    Rename to `ivfit` and update requirements.txt for patched Bokeh

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
-setup(name="lmf_gui",
+setup(name="ivfit",
       version="0.0.1-beta",
       description="An interactive 1D fitting toolkit",
       author="Jonathan Okasinski",
-      packages=['lmf_gui'])
+      packages=['ivfit'])
